25fe1a07a8713f1ce9a7f2b8e13e5e5e47a42506
Kaleb Lape <kaleb@testdouble.com>
2018-09-13 17:10:42 -0500

    Move hook scripts outside of stow packages.
    I realized as I was adding a stow ignore file that there could be other
    issues/conflicts with the packages themselves. Seems safer to just keep
    those dirs clean.

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ set -eux
 
 for dir in */
 do
-  test -f $dir/before.sh && bash $dir/before.sh
-  stow --target=$HOME $dir
-  test -f $dir/after.sh && bash $dir/after.sh
+  stow -v --target=$HOME $dir
+  test -f "$(basename $dir)_after".sh && bash "$(basename $dir)_after".sh
 done
