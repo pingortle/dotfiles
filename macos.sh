466388add55ce15424c63bff2b96e0e4fcca6456
Kaleb Lape <kaleb@testdouble.com>
2018-09-14 10:20:52 -0500

    Add my precious three finger swipe settings.

---
+++ macos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Inspired by https://mths.be/macos
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Trackpad Settings
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+
