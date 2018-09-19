4d32ed9eee4a663a2a2c42536472ac2ff72014e7
Kaleb Lape <pingortle@gmail.com>
2018-09-19 10:07:29 -0500

    Refactor git config.

--- git_after.sh
+++
@@ -1,7 +0,0 @@
-#!bash
-
-git config --global core.excludesfile ~/.gitignore_global
-git config --global init.templatedir '~/.git_template'
-git config --global alias.ctags '!.git/hooks/ctags'
-git config --global alias.commit-date '!git commit -m "$(date \'+%Y-%m-%d\')"'
-
