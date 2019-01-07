78a44bc99cacdb333211cd6647c25533dd345905
Kaleb Lape <pingortle@gmail.com>
2019-01-07 09:07:06 -0600

    Improve brew installer.

--- brew.sh
+++ brew.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install brew
-which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+command -v brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Freshen brew
 brew update
